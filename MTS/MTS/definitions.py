0f55de0b28f3f41f7eb5b7113d730a4bef91186e
sharwari.shinde@dynpro.com <sharwari.shinde@dynpro.com>
2025-04-23 09:37:59 +0000

    added comma

--- MTS/MTS/definitions.py
+++ MTS/MTS/definitions.py
@@ -8,7 +8,7 @@ from .dbt_cloud_trigger import run_dbt_from_dagster
 defs = Definitions(
     assets=[my_dbt_project_dbt_assets],
     schedules=schedules,
-    jobs=[run_dbt_from_dagster]
+    jobs=[run_dbt_from_dagster],
     resources={
         "dbt": DbtCliResource(project_dir=my_dbt_project_project),
     },
