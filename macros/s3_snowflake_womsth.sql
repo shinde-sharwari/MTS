00eeff671488b58b424e2a4d4a37b1d5b9ce5d17
Sharwari Shinde <sharwari.shinde@dynpro.com>
2025-05-02 11:03:55 +0000

    new added

--- macros/s3_snowflake_womsth.sql
+++ macros/s3_snowflake_womsth.sql
@@ -1,8 +1,18 @@
+-- -- macros/mts_bronze_womsth_data.sql
+-- {% macro mts_bronze_womsth_data() %}
+--     {% do run_query(
+--         "COPY INTO BOOTCAMP.MTS_BRONZE.T_BRZ_WORK_ORDER_HEADER_WOMSTH
+--             FROM @MTS_RAW.STG_MTS_IBMI_S3_RAW_EXT/womsth
+--             FILE_FORMAT = (TYPE = 'CSV' FIELD_OPTIONALLY_ENCLOSED_BY='\"' SKIP_HEADER=1)"
+--     ) %}
+-- {% endmacro %}
+
+
 -- macros/mts_bronze_womsth_data.sql
 {% macro mts_bronze_womsth_data() %}
     {% do run_query(
-        "COPY INTO BOOTCAMP.MTS_BRONZE.T_BRZ_WORK_ORDER_HEADER_WOMSTH
-            FROM @MTS_RAW.STG_MTS_IBMI_S3_RAW_EXT/womsth
+        "COPY INTO BOOTCAMP.MTS_BRONZE.T_BRZ_STORE_MASTER_STMAST
+            FROM @MTS_RAW.STG_MTS_IBMI_S3_RAW_EXT/in
             FILE_FORMAT = (TYPE = 'CSV' FIELD_OPTIONALLY_ENCLOSED_BY='\"' SKIP_HEADER=1)"
     ) %}
 {% endmacro %}
